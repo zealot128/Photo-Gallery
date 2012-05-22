489281936c7f8724d7ee73a2a2eb009237e00b84
Admin <admin@pludoni.de>
2012-05-22 10:59:30 +0200

    unicorn config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,6 +2,12 @@ worker_processes 4
 preload_app true
 timeout 30
 listen 9001
+working_directory "/apps/pics/prod/current"
+pid "/apps/pics/prod/current/tmp/pids/unicorn.pid"
+
+
+GC.respond_to?(:copy_on_write_friendly=) and
+  GC.copy_on_write_friendly = true
 
 after_fork do |server, worker|
     ActiveRecord::Base.establish_connection
