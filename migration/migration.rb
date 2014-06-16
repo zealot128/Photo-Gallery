00abd324ebd9002a7c83da64a296a50f58521f20
Stefan Wienert <stefan.wienert@pludoni.de>
2014-06-16 23:10:38 +0200

    cleanup

--- migration/migration.rb
+++ migration/migration.rb
@@ -2,9 +2,12 @@ mapping = Hash[YAML.load_file('migration/picsmapping.yml')]
 Photo.slow_callbacks = false
 Photo.find_each do |photo|
   path_component = mapping[photo.id]
-  photo.file = File.open Rails.root.join("public/photos/original#{path_component}")
+  path = Rails.root.join("public/photos/original#{path_component}")
+  photo.file = File.open path
   photo.set_metadata
-  photo.save
+  if photo.save
+    File.unlink path
+  end
 end
 Photo.slow_callbacks = true
 Day.find_each { |i| i.update_me }
