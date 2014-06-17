8650afee62ab703905dd505cebf44a3f55e45f1f
Stefan Wienert <stefan.wienert@pludoni.de>
2014-06-17 09:54:53 +0200

    ...

--- app/models/photo_metadata.rb
+++ app/models/photo_metadata.rb
@@ -27,19 +27,14 @@ module PhotoMetadata
 
 
   def set_metadata
-
     return if !file.present?
-    puts 'File present'
-    self.fingerprint =  Phashion::Image.new(file.path).fingerprint rescue nil
-    puts 'Fingerprint'
+    # self.fingerprint =  Phashion::Image.new(file.path).fingerprint rescue nil
     self.meta_data.merge! get_exif.exif.reduce({}){|a,e|a.merge(e)}.except(:user_comment).stringify_keys rescue nil
-    puts 'Metadata'
     self.meta_data_will_change!
     if Photo.slow_callbacks
       set_top_colors
     end
     self.save
-    puts 'Save'
   end
 
   def set_top_colors
