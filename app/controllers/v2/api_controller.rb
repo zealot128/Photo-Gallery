6bf25fc1c22f51549a4ec3c0cec3c1ca68c23107
Stefan Wienert <stefan.wienert@pludoni.de>
2016-05-06 22:23:06 +0800

    Some more testfixes

--- app/controllers/v2/api_controller.rb
+++ app/controllers/v2/api_controller.rb
@@ -1,5 +1,6 @@
 class V2::ApiController < ApplicationController
   before_filter :login_required
+
   def tags
     tags = ActsAsTaggableOn::Tag.order(:name).as_json
     render json: tags
---
+++ spec/controllers/v2/api_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe V2::ApiController do
+  before :each do
+    @controller.stub :current_user => User.create!(username: "stefan",
+                                                  password: "123123123",
+                                                  email: "info@example.com",
+                                                  password_confirmation: "123123123")
+  end
+  let :photo do
+    Photo.slow_callbacks = false
+    p = Photo.new shot_at: "2012-02-01"
+    p.save! validate: false
+    p
+  end
+
+  context "bulk update" do
+    specify "New Share onthefly has higher priority" do
+      share = Share.create(name: 'meetup2016')
+      post :bulk_update, file_ids: [photo.id],
+        tag_ids: [], new_tag: 'foobar',
+        share_ids: [share.id], new_share: 'newshare123'
+      photo.shares.map(&:name).sort.should be == ['meetup2016', 'newshare123']
+      photo.tag_list.should be == ['foobar']
+
+      get :tags
+      JSON.load(response.body).count.should be == 1
+
+      get :shares
+      JSON.load(response.body).count.should be == 2
+    end
+  end
+end
