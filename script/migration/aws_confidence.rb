72d5a3895e6fea3077177f0dd6e7e675d54bac98
Stefan Wienert <info@stefanwienert.de>
2016-12-12 20:08:46 +0100

    migration of existing images to confidence

---
+++ script/migration/aws_confidence.rb
@@ -0,0 +1,19 @@
+
+def run(next_token)
+  next_token = nil
+  loop do
+    print 'x'
+    response = RekognitionClient.collection(max_results: 1000, next_token: next_token)
+    response.faces.each do |face|
+      print '.'
+      ImageFace.where(confidence: nil, aws_id: face.face_id).update_all confidence: face.confidence
+    end
+    if response.next_token
+      next_token = response.next_token
+    else
+      break
+    end
+  end
+end
+
+run(nil)
