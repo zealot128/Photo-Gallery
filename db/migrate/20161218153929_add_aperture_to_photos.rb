15fd727594e558d4e3efa01d4751fe315843f5f0
Stefan Wienert <info@stefanwienert.de>
2016-12-19 10:26:41 +0100

    Fix migration for rational apertures

--- db/migrate/20161218153929_add_aperture_to_photos.rb
+++ db/migrate/20161218153929_add_aperture_to_photos.rb
@@ -8,7 +8,11 @@ class AddApertureToPhotos < ActiveRecord::Migration[5.0]
   def data
     Photo.pluck("id, (regexp_replace(meta_data::text, '\\\\u0000', '', 'g'))::json->'f_number'").each do |id, f|
       unless f.nil?
-        Photo.where(id: id).update_all aperture: f
+        new_val = case f
+                  when %r{(\d+)/(\d+)} then Rational($1.to_i, $2.to_i).to_f
+                  else f.to_f
+                  end
+        Photo.where(id: id).update_all aperture: new_val
       end
     end
   end
