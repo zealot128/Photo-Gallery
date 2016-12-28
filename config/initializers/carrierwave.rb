01dd3e02561108c85620ee714720258549c48e15
Stefan Wienert <info@stefanwienert.de>
2016-12-28 21:52:01 +0100

    Didnt thought about undefined settings

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,22 @@
 Rails.application.config.to_prepare do
-  time_zone = Setting.find_by_var('default_timezone').value
+  s = ->(key) { setting = Setting.find_by_var(key)
+                if setting
+                  setting.value
+                else
+                  Setting[key]
+                end
+  }
+  time_zone = s.('default_timezone')
   Rails.configuration.time_zone = time_zone
   Time.zone = ActiveSupport::TimeZone[Rails.configuration.time_zone]
   CarrierWave.configure do |config|
     config.aws_credentials = {
-      :access_key_id      => Setting.find_by_var( 'aws.access_key_id').value,
-      :secret_access_key  => Setting.find_by_var( 'aws.access_key_secret').value,
-      :region             => Setting.find_by_var( 'aws.region').value,
+      :access_key_id      => s.( 'aws.access_key_id'),
+      :secret_access_key  => s.( 'aws.access_key_secret'),
+      :region             => s.( 'aws.region'),
     }
-    config.aws_acl    = Setting.find_by_var( 'aws.acl').value
-    config.aws_bucket = Setting.find_by_var( 'aws.bucket').value
+    config.aws_acl    = s.('aws.acl')
+    config.aws_bucket = s.('aws.bucket')
     config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
     config.aws_attributes = {
       expires: 1.week.from_now.httpdate,
