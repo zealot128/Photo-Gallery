8e4fefb881970df671eedab0a2971284fd704557
Stefan Wienert <info@stefanwienert.de>
2016-12-28 21:49:14 +0100

    Settings in initializer doesnt work

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,20 @@
-CarrierWave.configure do |config|
-  config.aws_credentials = {
-    :access_key_id      => Setting['aws.access_key_id'],
-    :secret_access_key  => Setting['aws.access_key_secret'],
-    :region             => Setting['aws.region'],
-  }
-  config.aws_acl    = Setting['aws.acl']
-  config.aws_bucket = Setting['aws.bucket']
-  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
-  config.aws_attributes = {
-    expires: 1.week.from_now.httpdate,
-    cache_control: 'max-age=604800'
-  }
-end
+Rails.application.config.to_prepare do
+  time_zone = Setting.find_by_var('default_timezone').value
+  Rails.configuration.time_zone = time_zone
+  Time.zone = ActiveSupport::TimeZone[Rails.configuration.time_zone]
+  CarrierWave.configure do |config|
+    config.aws_credentials = {
+      :access_key_id      => Setting.find_by_var( 'aws.access_key_id').value,
+      :secret_access_key  => Setting.find_by_var( 'aws.access_key_secret').value,
+      :region             => Setting.find_by_var( 'aws.region').value,
+    }
+    config.aws_acl    = Setting.find_by_var( 'aws.acl').value
+    config.aws_bucket = Setting.find_by_var( 'aws.bucket').value
+    config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
+    config.aws_attributes = {
+      expires: 1.week.from_now.httpdate,
+      cache_control: 'max-age=604800'
+    }
+  end
 
+end
--- config/initializers/settings.rb
+++
@@ -1,2 +0,0 @@
-Rails.configuration.time_zone = Setting['default_timezone']
-Time.zone = ActiveSupport::TimeZone[Rails.configuration.time_zone]
