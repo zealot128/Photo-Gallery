635038c763b0ce6d4ad74d86dc8bf18fbee1bfcd
Stefan Wienert <info@stefanwienert.de>
2017-11-23 00:22:21 +0100

    Fix deployment

--- config/initializers/logging.rb
+++ config/initializers/logging.rb
@@ -1,15 +1,15 @@
-if !Rails.env.test? && !Rails.env.development?
-  begin
-    LoggingEntry.count
-    Rails.logger = LoggingEntry::LoggerProxy.new(nil, 0)
-    ActiveSupport::Reloader.to_prepare do
-      Rails.logger = LoggingEntry::LoggerProxy.new(nil, 0)
-    end
-  rescue StandardError
-  end
-end
-require 'silencer/logger'
+# if !Rails.env.test? && !Rails.env.development?
+#   begin
+#     LoggingEntry.count
+#     Rails.logger = LoggingEntry::LoggerProxy.new(nil, 0)
+#     ActiveSupport::Reloader.to_prepare do
+#       Rails.logger = LoggingEntry::LoggerProxy.new(nil, 0)
+#     end
+#   rescue StandardError
+#   end
+# end
+# require 'silencer/logger'
 
-Rails.application.configure do
-  config.middleware.swap Rails::Rack::Logger, Silencer::Logger, silence: [%r{(^/assets/|^/photos)}]
-end
+# Rails.application.configure do
+#   config.middleware.swap Rails::Rack::Logger, Silencer::Logger, silence: [%r{(^/assets/|^/photos)}]
+# end
