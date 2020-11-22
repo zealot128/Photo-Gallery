618b59e09b30d385a5d5c463ca305fcce62235c1
Stefan Wienert <info@stefanwienert.de>
2020-11-22 22:52:04 +0100

    Docker WIP

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
 
 require "readline"
-puts "Input password for user 'share': (password will be displayed)"
-buf = Readline.readline("> ", true).strip
 
-User.create! email: "admin@example.com", password: buf, password_confirmation: buf, username: "share", admin: true
+password = ENV['INITIAL_ADMIN_PASSWORD'].presence
+
+unless password
+  puts "Input password for user 'share': (password will be displayed)"
+  password = Readline.readline("> ", true).strip
+end
+
+User.create! email: "admin@example.com", password: buf, password_confirmation: buf, username: "admin", admin: true
 Share.create! name: "Public"
